9ff1e50c67f82052157911f97f559f9bda0e1cf3
Carlos Henrique <carloshenrique@MacBook-Pro-de-Carlos.local>
2025-06-09 20:36:43 -0400

    feat: Inicia estrutura do App do Usuário (PWA)

---
+++ APP/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    // Define a porta para o servidor de desenvolvimento
+    port: 3000,
+  }
+})
