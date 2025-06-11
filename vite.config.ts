e85df6108d6ac2936275c478e9e20c6c5ed04cd6
almeidasinop <suporteti.snp@gmail.com>
2025-06-11 09:18:40 -0400

    fix: remove entrada pwa.html e centraliza via MasterApp

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,11 @@ import { defineConfig } from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  // Configuração para o build de múltiplas páginas
-  build: {
-    rollupOptions: {
-      input: {
-        admin: path.resolve(__dirname, 'index.html'),
-      },
     },
   },
 })
