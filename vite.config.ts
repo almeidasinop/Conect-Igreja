239c1f0725607af5f21d713743132bf8e300b85a
almeidasinop <suporteti.snp@gmail.com>
2025-06-11 08:59:53 -0400

    versel

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         admin: path.resolve(__dirname, 'index.html'),
-        app: path.resolve(__dirname, 'pwa.html'),
       },
     },
   },
